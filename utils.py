054ef916e415ae0f6abff72636c04fb03ef474d4
yt <yuto-200504@i.softbank.jp>
2025-09-30 17:58:47 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -21,27 +21,6 @@ def preprocess_state(state, device):
     state_tensor = torch.tensor(state, dtype=torch.float32, device=device).permute(2, 0, 1) / 255.0
     return state_tensor.unsqueeze(0)
 
-class RunningMeanStd:
-    def __init__(self, shape=(), epsilon=1e-8):
-        self.mean = np.zeros(shape, 'float64')
-        self.var = np.ones(shape, 'float64')
-        self.count = epsilon
-
-    def update(self, x):
-        batch_mean = np.mean(x, axis=0)
-        batch_var = np.var(x, axis=0)
-        batch_count = x.shape[0]
-        delta = batch_mean - self.mean
-        tot_count = self.count + batch_count
-        new_mean = self.mean + delta * batch_count / tot_count
-        m_a = self.var * self.count
-        m_b = batch_var * batch_count
-        m_2 = m_a + m_b + np.square(delta) * self.count * batch_count / tot_count
-        new_var = m_2 / tot_count
-        self.mean = new_mean
-        self.var = new_var
-        self.count = tot_count
-
 def calculate_log_pi(log_stds, noises, actions):
     log_pis = -0.5 * math.log(2 * math.pi) * log_stds.size(-1) - log_stds.sum(dim=-1, keepdim=True) - (0.5 * noises.pow(2)).sum(dim=-1, keepdim=True)
     log_pis -= torch.log(1 - actions.pow(2) + 1e-6).sum(dim=-1, keepdim=True)
